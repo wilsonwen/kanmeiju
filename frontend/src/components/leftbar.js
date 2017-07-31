600e7f40e7f8b77f0677feeaaf129156a82c1c23
wilsonwen <wilson.wen.chn@gmail.com>
2017-07-31 21:55:50 +0800

    Collapse nav bar while link clicked on mobile

--- frontend/src/components/leftbar.js
+++ frontend/src/components/leftbar.js
@@ -3,6 +3,16 @@ import { Link } from 'react-router';
 import './leftbar.css'
 
 class Leftbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.collapse = this.collapse.bind(this);
+  }
+
+  collapse(event) {
+    this.refs.button.click();
+  }
+
   render() {
   	return (
   	  <div id="leftbar">
@@ -41,7 +51,7 @@ class Leftbar extends Component {
             <div className="container">
               <div className="navbar-header">
                 
-                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar2">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar2" ref="button">
                   <span className="sr-only"></span>
                   <span className="icon-bar"></span>
                   <span className="icon-bar"></span>
@@ -51,18 +61,17 @@ class Leftbar extends Component {
                 导航
                 </div>
               </div>
-
+ 
 
               <div id="sidebar2" className="navbar-collapse collapse">
                 <ul className="nav navbar-nav">
-                  <li><Link to='/latest'>最新更新</Link></li>
-                  <li><Link to='/hot'>人气</Link></li>
-                  <li><Link to='/top'>好评</Link></li>
-                  <li><Link to='/albumlist'>合集</Link></li>
-                  <li><Link to='/english'>英剧</Link></li>
-                  <li><Link to='/show'>综艺</Link></li>
-                  <li><Link to='/documentry'>纪录片</Link></li>
-                  
+                  <li><Link to='/latest' onClick={this.collapse}>最新更新</Link></li>
+                  <li><Link to='/hot' onClick={this.collapse}>人气</Link></li>
+                  <li><Link to='/top' onClick={this.collapse}>好评</Link></li>
+                  <li><Link to='/albumlist' onClick={this.collapse}>合集</Link></li>
+                  <li><Link to='/english' onClick={this.collapse}>英剧</Link></li>
+                  <li><Link to='/show' onClick={this.collapse}>综艺</Link></li>
+                  <li><Link to='/documentry' onClick={this.collapse}>纪录片</Link></li>
                 </ul>
               </div>
             </div>
