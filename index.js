d9dad349aaa07d972c80f5d08cb0e21b78052ca6
wilsonwen <wilson.wen.chn@gmail.com>
2017-08-23 20:16:28 +0800

    round-robin tokens to avoid ban

--- index.js
+++ index.js
@@ -29,6 +29,29 @@ app.use(function(req, res, next) {
  */
 SERVER = "http://api.rr.tv";
 SECRET_KEY = "clientSecret=08a30ffbc8004c9a916110683aab0060";
+TOKENS = [
+    '99ce6f3f7ff840e586958770472ec893',
+    '37570aa5dabe44219ab8a13986778390',
+    '6f8443b2d6c54d39bf7ab780bb5df3b2',
+    '3739e42b649948febad6dbfeaeecddfe',
+    'ffeb43e117d744e6822c3fe6abc12dea',
+    '5f6d0d3a49e5439e8492515c49b797c5',
+    '41d74adccd4b46389882242949e82cea',
+    'ff750a61e2684b7d9d4f783b4e3b14b1',
+    'f703dcbae2fb42889e8b111829b36167',
+    '9aea227ebb224a8b9bea347c13192706',
+    '827af86209464b038552a87f1cd546b2',
+    'bfc156b8c9ce48d98f2f9e48868130b3',
+    'c0a60ecf198d458d83b475e50b8d7893',
+    '22f6e8f0fac049849ce6db26535ecf6d',
+    'cce5bae0fcdd446fa87f0e61d86cd345',
+    'd1f33f20d83441228adf791215308d6d',
+    '803db2c8c9de400a9e90f75c2926d98a',
+    'b7c4f78d734f48fb871b3c7667f32019',
+    '99b3109e62fa4f1eb449182f5428cb84',
+    '10347e508c5946ec9116e3044b728f01'
+];
+Token_iterator = 0;
 FAKE_HEADERS = {
     "clientType": "android_%E8%B1%8C%E8%B1%86%E8%8D%9A",
     "clientVersion": "3.5.3.1",
@@ -155,6 +178,10 @@ var M3u8 = mongoose.model('m3u8', m3u8Schema);
 app.get('/api/m3u8/:episodeSid', function(req, res) {
     var api = SERVER + '/video/findM3u8ByEpisodeSid'
     
+    // round-robin tokens to avoid ban
+    FAKE_HEADERS['token'] = TOKENS[(Token_iterator++)%TOKENS.length];
+    console.log(FAKE_HEADERS['token']);
+
     // calculate signature
     var headers = FAKE_HEADERS;
     var body = {}
@@ -186,7 +213,6 @@ app.get('/api/m3u8/:episodeSid', function(req, res) {
             
             // Fetch remote data and set k,v in callback
             getJSON(api, body, function(json) {
-                console.log(json)
                 res.send(json);
                 client.set(key, json);
                 client.expire(key, 3500);
